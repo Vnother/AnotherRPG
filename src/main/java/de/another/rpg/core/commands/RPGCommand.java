bb5cf1ccf95f3817ee72dd1f8b9ab26e896e3e64
Another <88779024+Vnother@users.noreply.github.com>
2026-01-18 20:28:40 +0100

    basic ui component added

--- src/main/java/de/another/rpg/core/commands/RPGCommand.java
+++ src/main/java/de/another/rpg/core/commands/RPGCommand.java
@@ -2,13 +2,19 @@ package de.another.rpg.core.commands;
 
 import com.hypixel.hytale.component.Ref;
 import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.protocol.packets.interface_.CustomPageLifetime;
+import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
 import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.entity.entities.player.pages.CustomUIPage;
 import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+import de.another.rpg.ui.AnotherUI;
 import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
+
+import java.util.concurrent.CompletableFuture;
 
 public class RPGCommand extends AbstractPlayerCommand {
 
@@ -20,9 +26,19 @@ public class RPGCommand extends AbstractPlayerCommand {
     protected void execute(@NonNullDecl CommandContext commandContext, @NonNullDecl Store<EntityStore> store, @NonNullDecl Ref<EntityStore> ref, @NonNullDecl PlayerRef playerRef, @NonNullDecl World world) {
         Player player = store.getComponent(ref, Player.getComponentType());
         if (player != null) {
-            // Re-enable the UI
-            //SkillTreeUI ui = new SkillTreeUI(playerRef);
-            //player.getPageManager().openCustomPage(ref, store, ui);
+
+            //assert world != null;
+            //assert playerRef != null;
+
+           CompletableFuture.runAsync(() -> {
+               CustomUIPage page = player.getPageManager().getCustomPage();
+               if (page == null) {
+                   page = new AnotherUI(playerRef, CustomPageLifetime.CanDismiss);
+                   player.getPageManager().openCustomPage(ref, store, page);
+               }
+
+               playerRef.sendMessage(Message.raw("UI Page Shown"));
+           }, world);
         }
     }
 }
